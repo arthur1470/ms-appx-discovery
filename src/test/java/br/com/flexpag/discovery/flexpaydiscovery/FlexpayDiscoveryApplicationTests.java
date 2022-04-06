0a26eb0a0a6ce9e0f938404b009d1dfd1c4ea5c6
Arthur Marques <arthur.l2@hotmail.com>
2022-04-05 22:25:57 -0300

    Spring cloud discovery

---
+++ src/test/java/br/com/flexpag/discovery/flexpaydiscovery/FlexpayDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flexpag.discovery.flexpaydiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexpayDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
